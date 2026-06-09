8461434f3b4b4d82a57548515df36b97072cd816
licococ <molly71424@gmail.com>
2026-06-09 21:49:24 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ const products = [
     { id: 1, name: "法式盛夏芒果塔", price: 160, category: "法式塔類", img: "images/images (2).jpg" },
     { id: 2, name: "小山園宇治抹茶塔", price: 150, category: "法式塔類", img: "images/images (3).jpg" },
     { id: 3, name: "經典草莓戚風蛋糕", price: 240, category: "招牌蛋糕", img: "images/images (4).jpg" },
-    { id: 4, name: "焦糖海鹽磅蛋糕", price: 120, category: "常溫點心", img: "images/images (5).jpg" },
-    { id: 5, name: "法式檸檬塔", price: 140, category: "法式塔類", img: "images/images (6).jpg" },
-    { id: 6, name: "伯爵紅茶瑪德蓮", price: 65, category: "常溫點心", img: "images/images (7).jpg" },
-    { id: 7, name: "頂級巧克力慕斯", price: 180, category: "招牌蛋糕", img: "images/images (8).jpg" }
+    { id: 4, name: "海鹽焦糖巴斯克乳酪蛋糕", price: 120, category: "常溫點心", img: "images/800x.jpg" },
+    { id: 5, name: "法式經典香草可麗露 (2入)", price: 140, category: "法式塔類", img: "images/images (8).jpg" },
+    { id: 6, name: "靜岡抹茶瑪德蓮 (3入)", price: 65, category: "常溫點心", img: "images/images (7).jpg" },
+    { id: 7, name: "比利時濃郁榛果黑巧蛋糕", price: 180, category: "招牌蛋糕", img: "images/images (5).jpg" }
 ];
 
 // 頁面切換核心邏輯

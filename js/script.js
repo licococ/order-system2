481fbec93f1d963fbb21fb88687ee5b8f879e153
licococ <molly71424@gmail.com>
2026-06-10 13:57:22 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ let currentUser = null;
         localStorage.setItem("user_sweet@123", "123");
 
         const products = [
-            { id: 1, name: "法式盛夏芒果塔", price: 160, category: "法式塔類", img: "images/images (2).jpg" },
-            { id: 2, name: "小山園宇治抹茶塔", price: 150, category: "法式塔類", img: "images/images (3).jpg" },
-            { id: 3, name: "炙燒焦糖檸檬塔", price: 140, category: "法式塔類", img: "images/800x.jpg" },
-            { id: 4, name: "經典草莓戚風蛋糕", price: 240, category: "招牌蛋糕", img: "images/images (4).jpg" },
-            { id: 5, name: "比利時濃郁榛果黑巧蛋糕", price: 220, category: "招牌蛋糕", img: "images/images (5).jpg" },
-            { id: 6, name: "海鹽焦糖巴斯克乳酪蛋糕", price: 180, category: "招牌蛋糕", img: "images/images (6).jpg" },
-            { id: 7, name: "靜岡抹茶瑪德蓮 (3入)", price: 110, category: "常溫點心", img: "images/images (7).jpg" },
-            { id: 8, name: "法式經典香草可麗露 (2入)", price: 130, category: "常溫點心", img: "images/images (8).jpg" }
+            { id: 1, name: "法式盛夏芒果塔", price: 160, category: "法式塔類", img: "images/mango.jpg" },
+            { id: 2, name: "小山園宇治抹茶塔", price: 150, category: "法式塔類", img: "images/matcha.jpg" },
+            { id: 3, name: "炙燒焦糖檸檬塔", price: 140, category: "法式塔類", img: "images/lemon_tart.jpg" },
+            { id: 4, name: "經典草莓戚風蛋糕", price: 240, category: "招牌蛋糕", img: "images/cake_berry.jpg" },
+            { id: 5, name: "比利時濃郁榛果黑巧蛋糕", price: 220, category: "招牌蛋糕", img: "images/cake_cocoa.jpg" },
+            { id: 6, name: "海鹽焦糖巴斯克乳酪蛋糕", price: 180, category: "招牌蛋糕", img: "images/cake_cheese.jpg" },
+            { id: 7, name: "靜岡抹茶瑪德蓮 (3入)", price: 110, category: "常溫點心", img: "images/madeleine.jpg" },
+            { id: 8, name: "法式經典香草可麗露 (2入)", price: 130, category: "常溫點心", img: "images/canelet.jpg" }
         ];
 
         function switchSystem(systemType) {
